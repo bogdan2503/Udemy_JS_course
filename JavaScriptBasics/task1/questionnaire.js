739d5245101b4b2c1e8880854e291e34985b931f
bogdan2503 <korovjakbogdan@gmail.com>
2022-06-05 01:04:52 +0300

    final

--- JavaScriptBasics/task1/questionnaire.js
+++ JavaScriptBasics/task1/questionnaire.js
@@ -1,1 +1,5 @@
-alert ('Hello')
+let firstname = prompt ("What is your name?");
+let surname = prompt ("What is your surname?");
+let age = prompt ("How old are you?");
+
+console.log (`User ${firstname} ${surname} is ${age}`)
