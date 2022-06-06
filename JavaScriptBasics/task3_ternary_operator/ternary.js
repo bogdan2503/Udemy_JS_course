32f2fe0aaafc739c0537daab0665251fb497634d
bogdan2503 <korovjakbogdan@gmail.com>
2022-06-07 01:10:43 +0300

    task3_ternary_operator

---
+++ JavaScriptBasics/task3_ternary_operator/ternary.js
@@ -0,0 +1,4 @@
+// let age = prompt ("enter your age")
+
+prompt ("enter your age") >= 18 ? alert ("you can by alcohol") : alert ("you can not buy the alcohol, you are to young")
+prompt ("enter your age") >= 18 ? console.log ("you can by alcohol") : console.log ("you can not buy the alcohol, you are to young")
